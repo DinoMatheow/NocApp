c947f105f0c7557991289590899b53d381038783
Matheow2 <matius152014@gmail.com>
2026-04-20 18:11:50 -0500

    Archivos adjuntos

--- src/presentation/email/email.service.ts
+++ src/presentation/email/email.service.ts
@@ -2,11 +2,19 @@ import  nodemailer from "nodemailer";
 import { envs } from "../../config/plugins/envs.plugin";
 
 interface SendEmailOptions {
-    to: string;
+    to: string | string[];
     subject: string;
     htmlBody: string;
+    attachements?: Attachment[]; 
 
 }
+
+interface Attachment {
+    filename: string;
+    path: string;
+
+}
+
 
 
 export class EmailService {
@@ -20,13 +28,15 @@ export class EmailService {
 
 
     async sendEmail(options:SendEmailOptions):Promise<boolean>{
-        const { to, subject, htmlBody } = options;
+        const { to, subject, htmlBody, attachements = [] } = options;
 
         try {   
             const sendInformation = await this.transporter.sendMail({
                 to: to,
                 subject: subject,
                 html: htmlBody,
+                attachments: attachements
+
             });
 
             console.log("Email sent: ", sendInformation);
@@ -40,5 +50,19 @@ export class EmailService {
         }
 
 
+    }
+
+    SendEmailWithFileSystemLogs( to:string | string[] ){
+        const subject = 'Log del server';
+        const htmlBody = '<h1>Logs del server</h1><p>Adjunto encontrarás los logs del servidor.</p>';
+
+
+        const attachements: Attachment[] = [
+            {
+                filename: 'logs-all.log', path: './logs/logs-all.log'},
+            // {filename: 'logs-high.log', path: './logs/logs-error.log'},
+            // {filename: 'logs-medium.log', path: './logs/logs-warn.log'}
+        ];
+        return this.sendEmail({to, subject, htmlBody, attachements});
     }
 }
